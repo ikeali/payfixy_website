3be69e3c0710bb1a4ec42cfa5328cde1b0d11267
ikeali <ikechukwualisigwe01@gmail.com>
2024-11-21 13:25:22 -0800

    all

--- website/settings.py
+++ website/settings.py
@@ -100,7 +100,7 @@ CORS_ALLOW_ALL_ORIGINS = True
 
 
 
-CORS_ALLOWED_ORIGINS = ["http://localhost:3001",]
+CORS_ALLOWED_ORIGINS = ["http://localhost:3001", "http://localhost:3001"]
 
 if config("ENVIRONMENT") == "DEV":
     DATABASES = {
