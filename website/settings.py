77ea1808ef2c9c43ca7c639d13e7520ae0fd031f
ikeali <ikechukwualisigwe01@gmail.com>
2024-11-21 14:54:31 -0800

    all

--- website/settings.py
+++ website/settings.py
@@ -82,6 +82,7 @@ WSGI_APPLICATION = 'website.wsgi.application'
 
 CORS_ALLOW_ALL_ORIGINS = True
 
+CORS_ORIGIN_ALLOW_ALL = True
 
 
 # Database
@@ -100,8 +101,8 @@ CORS_ALLOW_ALL_ORIGINS = True
 
 
 
-# CORS_ALLOWED_ORIGINS = ["http://localhost:3000", "http://localhost:3001", "*"]
-CORS_ALLOWED_ORIGINS = ["*"]
+CORS_ALLOWED_ORIGINS = ["http://localhost:3000", "http://localhost:3001"]
+# CORS_ALLOWED_ORIGINS = ["*"]
 
 if config("ENVIRONMENT") == "DEV":
     DATABASES = {
