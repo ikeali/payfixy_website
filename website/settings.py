4e7f4f3cc9038aa9ad8df274cb4c516a864cc55c
ikeali <ikechukwualisigwe01@gmail.com>
2024-11-21 16:28:19 -0800

    all

--- website/settings.py
+++ website/settings.py
@@ -80,9 +80,36 @@ TEMPLATES = [
 WSGI_APPLICATION = 'website.wsgi.application'
 
 
-CORS_ALLOW_ALL_ORIGINS = True
+# CORS_ALLOW_ALL_ORIGINS = True
 
 CORS_ORIGIN_ALLOW_ALL = True
+
+CORS_ALLOW_METHODS = [
+'DELETE',
+'GET',
+'OPTIONS',
+'PATCH',
+'POST',
+'PUT',
+]
+CORS_ALLOW_HEADERS = [
+'accept',
+'accept-encoding',
+'authorization',
+'content-type',
+'dnt',
+'origin',
+'user-agent',
+'x-csrftoken',
+'x-requested-with',
+]
+CORS_ALLOW_ALL_ORIGINS = True
+
+CORS_ALLOW_CREDENTIALS = True
+
+
+
+
 
 
 # Database
