73e8f092fb9fcc825e6c1022f51a8f4bad002ec0
ikeali <ikechukwualisigwe01@gmail.com>
2024-11-28 12:08:00 -0800

    removed login otp

--- kyc/urls.py
+++ kyc/urls.py
@@ -6,7 +6,8 @@ from .views import BusinessOwnerViewSet
 
 router = DefaultRouter()
 router.register(r'business-owner', BusinessOwnerViewSet, basename='business-owner')
-
+router.register(r'kyc', KYCViewSet, basename='kyc')
+router.register(r'kyc-review', KYCReviewViewSet, basename='kyc-review')
 
 
 
@@ -15,8 +16,7 @@ urlpatterns = [
     path('', include(router.urls)),
     path('business-details/', BusinessDetailsView.as_view(), name='business-details'),
     path('business-documents/', BusinessDocumentView.as_view(), name='business-documents'),
+    # path('bank_account/', BankAccountView.as_view(), name='bank_account'),
     path('verify_account_number/', VerifyAccountNumberView.as_view(), name='account_number'),
     path('verify-bvn/', VerifyBVNView.as_view(), name="verify_bvn"),
-    path('kyc-summary/', KYCSummaryView.as_view(), name='kyc-summary'),
-
 ]
--- website/settings.py
+++ website/settings.py
@@ -255,6 +255,8 @@ STATIC_URL = 'static/'
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
+STATIC_ROOT = BASE_DIR / 'staticfiles'
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
