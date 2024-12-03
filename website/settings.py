ef2be12b1d65a45837f1079dfd48865064d18c6a
ikeali <ikechukwualisigwe01@gmail.com>
2024-12-03 15:16:32 -0800

    works
    works

--- website/settings.py
+++ website/settings.py
@@ -48,6 +48,16 @@ INSTALLED_APPS = [
     'kyc'
 ]
 
+
+CLOUDINARY_STORAGE = {
+    'CLOUD_NAME': config('CLOUDINARY_NAME'),
+    'API_KEY': config('CLOUDINARY_API_KEY'),
+    'API_SECRET': config('CLOUDINARY_SECRET_KEY')
+}
+
+DEFAULT_FILE_STORAGE = 'cloudinary_storage.storage.MediaCloudinaryStorage'
+
+
 MIDDLEWARE = [
     'corsheaders.middleware.CorsMiddleware',
     'django.middleware.common.CommonMiddleware',
